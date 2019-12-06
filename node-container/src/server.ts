7e187554c609d21daa683996ff148a88d8f66d8b
Michael Tajmajer <Michael.Tajmajer@Bentley.com>
2019-12-06 13:42:06 -0600

    Changed from linux to windows nanoserver container

--- node-container/src/server.ts
+++ node-container/src/server.ts
@@ -1,14 +1,19 @@
 import express from 'express';
+import os from 'os';
 
 // Constants
 const PORT = 8080;
 const HOST = "0.0.0.0";
 
+const osInfo = `OS: ${os.type()} Release: ${os.release()} HostName: ${os.hostname()}`;
+
 // App
 const app = express();
 app.get("/", (_req, res) => {
-    res.send("Hello from a container!\n");
+    res.send(`<div>Hello from a windows container!</br>${osInfo}</div>`);
 });
 
 app.listen(PORT, HOST);
+
+console.log(osInfo);
 console.log(`Running on http://${HOST}:${PORT}`);
